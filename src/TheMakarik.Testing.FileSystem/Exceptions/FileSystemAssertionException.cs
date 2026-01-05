e752af422281caa89e12d950da69a924399ff5c9
TheMakarik <themakarik@gmail.com>
2026-01-05 18:03:12 +0700

    Added default assertion exception
    # Conflicts:
    #	.gitignore

---
+++ src/TheMakarik.Testing.FileSystem/Exceptions/FileSystemAssertionException.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TheMakarik.Testing.FileSystem.Exceptions
+{
+    /// <summary>
+    /// Represent default exception that occurs then your file system assertion is not true.
+    /// </summary>
+    public sealed class FileSystemAssertionException : Exception
+    {
+        /// <summary>
+        /// Unparameterized constructor for <see cref="FileSystemAssertionException"/>
+        /// </summary>
+        public FileSystemAssertionException() : base() { }
+        
+        /// <summary>
+        /// Constructor for <see cref="FileSystemAssertionException"/> with exception message
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public FileSystemAssertionException(string message) : base(message) { }
+    }
+}
